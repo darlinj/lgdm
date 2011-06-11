35895b9b54d13f794f8222e7afd13c3278ceaf6b
joe <joe.darling@bt.com>
2011-06-11 22:34:24 +0100

    Add routing for servers index page

---
+++ spec/routing/servers_routes_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe ServersController, "routing" do
+
+  it "should route correctly" do
+    {:get  => '/servers'        }.should route_to(:controller => "servers", :action => "index")
+    #{:get  => '/'                         }.should route_to(:controller => "servers", :action => "index")
+    #{:get  => '/servers/new'    }.should route_to(:controller => "servers", :action => "new")
+    #{:get  => '/servers/1/edit' }.should route_to(:controller => "servers", :action => "edit", :id=>"1")
+    #{:put  => '/servers/1'      }.should route_to(:controller => "servers", :action => "update", :id=>"1")
+    #{:post => '/servers'        }.should route_to(:controller => "servers", :action => "create")
+  end
+
+end
