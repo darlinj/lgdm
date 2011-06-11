dd0e18a5a7ec948235a23d193f1c1b7910362800
joe <joe.darling@bt.com>
2011-06-11 08:01:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lgdm::Application.config.secret_token = 'f9478cbb481157141e38848b925b5542e2f01cfd455892ff293eafe96bee668ba436326cc8b2c52d04121db2acc1801ff10459133fcc21423a21b78fb7da6469'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lgdm::Application.config.session_store :cookie_store, :key => '_lgdm_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lgdm::Application.config.session_store :active_record_store
